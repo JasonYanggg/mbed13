3235fc71a12ce3f64f91341666545fb4a35e69c3
JasonYang <jason53240@gmail.com>
2020-06-09 00:42:09 +0800

    13_3

---
+++ 13_3_BBCar_Calibration/main.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+#include "bbcar.h"
+
+Ticker servo_ticker;
+PwmOut pin8(D8), pin9(D9);
+
+BBCar car(pin8, pin9, servo_ticker);
+
+int main()
+{
+    double pwm_table0[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
+    double speed_table0[] = {-15.790, -15.392, -14.594, -12.122, -6.220, 0.000, 5.822, 11.643, 14.594, 15.790, 16.189};
+    double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
+    double speed_table1[] = {-16.428, -16.189, -15.072, -11.723, -5.263, 0, 6.699, 12.201, 15.152, 16.189, 16.508};
+
+    // first and fourth argument: length of table
+    car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
+
+    while (1) {
+        car.goStraightCalib(5);
+        wait(5);
+        car.stop();
+        wait(5);
+    }
+}
